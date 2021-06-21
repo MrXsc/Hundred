9b7b5f8f0413e7d24787c0ff46976cd639fe3670
MrXsc <derekcs@qq.com>
2021-06-21 21:06:21 +0800

    Initial Commit

---
+++ Hundred/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hundred
+//
+//  Created by Shuo Xiao on 2021/6/21.
+//  Copyright © 2021 CutEnglishOneHundred. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
